078699d1e6a0295053cb9b29584ccae1d396f6dc
Pramodjena <pramoodjena@gmail.com>
2025-07-01 23:42:44 +0530

    mern updated

---
+++ Javascript/07_TruthyFalsy/script.js
@@ -0,0 +1,3 @@
+let a = prompt("enter first number");
+let b = prompt("enter second number");
+[a, b] = [b, a];
---
+++ Javascript/12_Ternary/script.js
@@ -0,0 +1,12 @@
+// const username = 5 > 2 ? "Pramod" : "SDE-III";
+
+// const gender = 'M'
+
+// debugger
+// const userMessage = `${gender.toLocaleLowerCase() === 'M' ? 'He' : 'She'} is a college student.`
+
+// console.log(userMessage);
+
+// const result = null ? "Pramod" : "" ? "12" : 0;
+
+// console.log(result);
